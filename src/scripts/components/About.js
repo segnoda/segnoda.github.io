2fccfce91d234ae6b3bf5244b0c1cbd5677e83f4
segnolin <segnolin@gmail.com>
2017-01-22 17:55:53 +0800

    revome components that directly operate DOM

--- src/scripts/components/About.js
+++ src/scripts/components/About.js
@@ -1,63 +1,15 @@
 import React from 'react';
 import $ from 'jquery';
 
-import AboutNav from './about/AboutNav';
 import Profile from './about/Profile';
 import Education from './about/Education';
 import Experience from './about/Experience';
 import Contact from './about/Contact';
 
 export default class About extends React.Component {
-    componentDidMount() {
-        this.handleSroll();
-    }
-    componentDidUpdate() {
-        this.handleSroll();
-    }
-    handleSroll() {
-        $(document).ready(function() {
-            $(document).on('scroll', onScroll);
-
-            $('a[href^="#"]').on('click', function(e) {
-                e.preventDefault();
-                $(document).off('scroll');
-
-                $('a').each(function() {
-                    $(this).removeClass('active');
-                });
-                $(this).addClass('active');
-
-                var target = this.hash,
-                    menu = target,
-                    $target = $(target);
-                $('html, body').stop().animate({
-                    'scrollTop': $target.offset().top
-                }, 800, 'swing', function() {
-                    window.location.hash = target;
-                    $(document).on('scroll', onScroll);
-                });
-            });
-        });
-
-        function onScroll(event) {
-            var scrollPos = $(document).scrollTop();
-            $('.about-nav a').each(function() {
-                var currLink = $(this),
-                    refElement = $(currLink.attr('href'));
-                if (refElement.position().top <= scrollPos && refElement.position().top + refElement.height() > scrollPos) {
-                    $('.about-nav a').removeClass('active');
-                    currLink.addClass('active');
-                }
-                else {
-                    currLink.removeClass('active');
-                }
-            });
-        }
-    }
     render() {
         return(
             <div className="about">
-                <AboutNav />
                 <Profile />
                 <Education />
                 <Experience />
